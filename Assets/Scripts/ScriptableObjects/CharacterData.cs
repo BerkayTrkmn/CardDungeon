0ef221a726f0bd90bc8dfeed5b8c0f202afe0e80
BerkayTürkmen <trkmnbrky@gmail.com>
2022-08-16 23:52:09 +0300

    +

---
+++ Assets/Scripts/ScriptableObjects/CharacterData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Character", menuName = "Data/CharacterData", order = 1)]
+
+public class CharacterData : CardData
+{
+    public int health;
+    public int attack;
+}
